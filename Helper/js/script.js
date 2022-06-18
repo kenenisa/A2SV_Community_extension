edfcbcbd7c13cd75f0b9e68ef68c0fe668e8bbf2
kenenisa <53818162+kenenisa@users.noreply.github.com>
2022-06-18 17:04:54 +0300

    helper script working

--- Helper/js/script.js
+++ Helper/js/script.js
@@ -1,6 +1,69 @@
-console.log("script Loaded");
+console.log("A2SV Script Loaded and Running...");
 console.log(document);
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-  console.log(document);
+localStorage.A2SV_timer = localStorage.A2SV_timer || new Date().getTime();
+//wait for submission result
+const result = (resolve) => {
+  const LookForResult = () => {
+    const success = document.getElementsByClassName("success__3Ai7")[0];
+    const error = document.getElementsByClassName("error__2Ft1")[0];
+    if (success || error) {
+      if (success) {
+        resolve(success);
+      }
+      // if (error) {
+      //   reject(error);
+      // }
+    } else {
+      setTimeout(LookForResult, 500);
+    }
+  };
+  LookForResult();
+};
+//wait for submission result
+const loaded = (resolve) => {
+  const Loading = () => {
+    const submitButton = document.querySelector('[data-cy="submit-code-btn"]');
+    if (submitButton) {
+      resolve(submitButton);
+    } else {
+      setTimeout(Loading, 500);
+    }
+  };
+  Loading();
+};
+// attach action when submit button is pressed
+loaded((submitButton) => {
+  submitButton.addEventListener("click", () => {
+    const [qId, qTitle] = document
+      .querySelector('[data-cy="question-title"]')
+      .innerHTML.split(". "); // question id and title
+    const user = JSON.parse(localStorage["GLOBAL_DATA:value"]).userStatus;
+    const username = user.username;
+    const asi = user.activeSessionId;
+    const lang = JSON.parse(localStorage.global_lang); // lang used
+    result(() => {
+      console.log("result obtained");
+      const code = localStorage[qId + "_" + asi + "_" + lang]; //code submitted
+      const submissions =
+        document.getElementsByClassName("ant-table-tbody")[0].children.length; //number of submissions
+      const rawInfo = document.getElementsByClassName("info__2oQ9");
+      const info = [rawInfo[0], rawInfo[1]].map((el) => {
+        return el.innerHTML.replace(/<[^>]*>/g, "").replace(/:[^;]*;/g, ": ");
+      }); // info thrown out by leet code
+      const time = new Date().getTime() - Number(localStorage.A2SV_timer); // time taken in ms
+      const data = {
+        code,
+        submissions,
+        info,
+        time,
+        qTitle,
+        qId,
+        username,
+        site: "leetcode",
+        lang,
+      };
+      localStorage.removeItem("A2SV_timer")
+      console.log(data);
+    })
+  });
 });
